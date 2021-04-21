8c455f5c0383a1c77ba7d348e0670881be8aed00
Vighneshwar Madas <vmadas@microsoft.com>
2021-04-21 06:00:18 +0100

    Create a simple GraphQL Web API

---
+++ simple-webapi-with-graphql/GraphQL/Queries/BooksQuery.cs
@@ -0,0 +1,9 @@
+﻿using simple_webapi_with_graphql.GraphQL.Types;
+
+namespace simple_webapi_with_graphql.GraphQL.Queries
+{
+    public class BooksQuery
+    {
+        public Book GetBook() => new Book() { Title = "C# in Depth", Author = "Job Skeet" };
+    }
+}
